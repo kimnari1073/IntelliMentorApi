60632426144da830271a23107899bd7280632d8d
kimnari1073 <nari1073@naver.com>
2024-05-10 20:21:47 +0900

    초기 세팅

---
+++ src/main/java/org/intelli/intellimentor/IntellimentorApplication.java
@@ -0,0 +1,13 @@
+package org.intelli.intellimentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellimentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellimentorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/intelli/intellimentor/IntellimentorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intelli.intellimentor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellimentorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
